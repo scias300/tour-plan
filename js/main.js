b8d9775c3cae153acaa91c42df02ac90ecbdad6a
scias300 <scias300@gmail.com>
2021-05-14 00:07:41 +0300

    create: adaptive for reviews

--- js/main.js
+++ js/main.js
@@ -11,3 +11,16 @@ const hotelSwiper = new Swiper('.hotel-slider', {
     onlyInViewport: true,
   },
 });
+const reviewsSwiper = new Swiper('.reviews-slider__container', {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+});
